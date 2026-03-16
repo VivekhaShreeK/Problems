c0a5809ef6090eae24bca90c717357a2dca097b7
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-03-16 06:56:38 +0530

    Time: 18 ms (95.59%) | Memory: 46.7 MB (100.00%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -1,11 +1,8 @@
 class Answer {
-
     int[] ans;
-
     public Answer() {
         ans = new int[3];
     }
-
     void put(int x) {
         if (x > ans[0]) {
             ans[2] = ans[1];
@@ -18,7 +15,6 @@ class Answer {
             ans[2] = x;
         }
     }
-
     List<Integer> get() {
         List<Integer> ret = new ArrayList<>();
         for (int num : ans) {
@@ -29,26 +25,21 @@ class Answer {
         return ret;
     }
 }
-
 class Solution {
-
     public int[] getBiggestThree(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] sum1 = new int[m + 1][n + 2];
         int[][] sum2 = new int[m + 1][n + 2];
-
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 sum1[i][j] = sum1[i - 1][j - 1] + grid[i - 1][j - 1];
                 sum2[i][j] = sum2[i - 1][j + 1] + grid[i - 1][j - 1];
             }
         }
-
         Answer ans = new Answer();
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                // a single cell is also a rhombus
                 ans.put(grid[i][j]);
                 for (int k = i + 2; k < m; k += 2) {
                     int ux = i;
@@ -75,7 +66,6 @@ class Solution {
                 }
             }
         }
-
         List<Integer> resultList = ans.get();
         int[] result = new int[resultList.size()];
         for (int i = 0; i < resultList.size(); i++) {
