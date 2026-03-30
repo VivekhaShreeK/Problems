ceace4d03df1b7c7f323e57c0eb0cc7b0735bfca
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-03-30 19:48:44 +0530

    Time: 6 ms (61.84%) | Memory: 47.9 MB (85.53%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+        int[] c1 = new int[256];
+        int[] c2 = new int[256];
+        for(int i = 0; i < s1.length(); i++) {
+            int a = (i & 1) << 7;
+            c1[a + s1.charAt(i)]++;
+            c2[a + s2.charAt(i)]++;
+        }
+        return Arrays.equals(c1, c2);
+    }
+}
