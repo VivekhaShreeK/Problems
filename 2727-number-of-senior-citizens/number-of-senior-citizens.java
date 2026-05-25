bf1389a569a3d4f5d7c85e3047c84bc91642c09f
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-05-25 16:09:17 +0530

    Time: 1 ms (71.84%) | Memory: 43.6 MB (97.74%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for(String d : details) {
+            if((((d.charAt(11) - '0') * 10) + (d.charAt(12) - '0')) > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
