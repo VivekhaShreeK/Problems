1999bd079157d6d5d72463d6c8f000ee5b93021f
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-05-25 15:58:08 +0530

    Time: 7 ms (45.52%) | Memory: 43.4 MB (17.13%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> hash = new HashMap<>();
+        for(char c : s.toCharArray()) {
+            hash.put(c, hash.getOrDefault(c, 0)+1);
+        }
+        int length = 0;
+        boolean odd = false;
+        for(int cnt : hash.values()) {
+            if(cnt % 2 == 0) {
+                length += cnt;
+            }
+            else {
+                length += cnt-1;
+                odd = true;
+            }
+        }
+        if(odd) {
+            length += 1;
+        }
+        return length;
+    }
+}
