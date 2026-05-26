8dc88852d05d4eaf45fe808a1c8d80031fda8124
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-05-26 20:38:55 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (81.71%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int l = 0, u = 0;
+        for(char ch : word.toCharArray()) {
+            if(Character.isLowerCase(ch)) {
+                l |= (1 << (ch - 'a'));
+            }
+            else {
+                u |= (1 << (ch - 'A'));
+            }
+        }
+        int a = l & u;
+        return Integer.bitCount(a);
+    }
+}
