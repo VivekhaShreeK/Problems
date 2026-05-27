131fd4678d2959191d3672f89cff5afd5e2efc3a
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-05-27 14:28:33 +0530

    Time: 11 ms (74.79%) | Memory: 48.5 MB (9.09%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        boolean[][] A = new boolean[2][27];
+        for(int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int idx = ch & 31;
+            int b = (ch >> 5) & 1;
+            A[b][idx] = b == 0 || !A[0][idx];
+        }
+        int res = 0;
+        for(int i = 1; i < 27; i++) {
+            if(A[0][i] && A[1][i])
+                res++;
+        }
+        return res;
+    }
+}
