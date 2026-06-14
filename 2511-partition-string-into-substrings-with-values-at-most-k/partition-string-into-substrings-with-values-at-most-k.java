aaee4eb6292a6f41aa5e1cd1d19c1d720473eeed
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-06-14 20:22:24 +0530

    Time: 37 ms (16.16%) | Memory: 47 MB (19.19%) - LeetSync

---
+++ 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    private boolean isOk(StringBuilder sb,int k){
+        return Long.parseLong(sb.toString())<=k;
+    }
+    public int minimumPartition(String s, int k) {
+        int count=0,pos=0;
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+             if((ch-'0')>k)return -1;
+             else if(!isOk(sb.append(ch),k)){
+                count++;
+                sb.delete(0,sb.length()-1);
+            }
+        }
+        return count+1;
+
+    }
+}
